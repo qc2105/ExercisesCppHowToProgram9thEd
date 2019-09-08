f7f6962eeb2df8e21155d8d7ae86ccb74ece1b88
Richard Qin <linuxqc@outlook.com>
2019-09-08 10:27:46 +0800

    5.19

---
+++ Chapter5-exercises-5.19/Chapter5-exercises-5.19.cpp
@@ -0,0 +1,39 @@
+// Chapter5-exercises-5.19.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+	double pi = 0;
+	double term = 0;
+	for (int i = 1; i <= 10000; i++)
+	{
+		if (i % 2 == 0)
+		{
+			term = -4.0 / (2.0 * i - 1.0);
+		}
+		else
+		{
+			term = 4.0 / (2.0 * i - 1.0);
+		}
+		pi += term;
+		std::cout << std::fixed;
+		std::cout << pi << std::endl;
+	}
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
