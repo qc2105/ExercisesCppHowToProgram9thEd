65be47f55eaf5c7e721748fe99a2a6cc4af58d06
Chuan Qin <qc2105@qq.com>
2020-06-01 21:52:58 +0800

    22.5

---
+++ 22.5/22.5.cpp
@@ -0,0 +1,27 @@
+// 22.5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "DeckOfCards.h"
+
+int main()
+{
+    DeckOfCards deckOfCards; // create DeckOfCards object
+    deckOfCards.deal();
+    std::cout << "--------------------------\n\n";
+    deckOfCards.shuffle(); // shuffle the cards in the deck
+    deckOfCards.deal(); // deal the cards in the deck
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
