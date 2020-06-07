c9075521dcf243df53560173697f990f9b3c01d2
Chuan Qin <qc2105@qq.com>
2020-06-07 17:16:28 +0800

    E10

---
+++ E10/E10.cpp
@@ -0,0 +1,32 @@
+// E10.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#define SUMARRY(Array, Size, sum)   for (size_t i = 0; i < Size; ++i) { sum += Array[i];}
+
+int main()
+{
+    double Array[10];
+    for (int i = 0; i < 10; ++i)
+    {
+        Array[i] = i;
+    }
+    
+    double sum = 0;
+    SUMARRY(Array, 10, sum);
+
+    std::cout << sum << std::endl;
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
