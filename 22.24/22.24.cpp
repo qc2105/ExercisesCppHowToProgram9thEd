d4266d6e9042f0944967ffb55416503722cce584
Chuan Qin <qc2105@qq.com>
2020-06-10 21:58:58 +0800

    22.24

---
+++ 22.24/22.24.cpp
@@ -0,0 +1,33 @@
+// 22.24.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+void printAscii(int code);
+
+int main()
+{
+    for (int i = 0; i <= 255; ++i)
+    {
+        std::cout << i << '\t';
+        printAscii(i);
+    }
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+void printAscii(int code)
+{
+    std::cout << static_cast<char>(code) << std::endl;
+}
