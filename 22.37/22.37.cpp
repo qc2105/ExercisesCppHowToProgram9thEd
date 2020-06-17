963ec0aa055d6077a77c1f80c58f63ccd32bacd7
Chuan Qin <qc2105@qq.com>
2020-06-17 23:07:15 +0800

    22.37

---
+++ 22.37/22.37.cpp
@@ -0,0 +1,47 @@
+// 22.37.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+bool compare(const char* str1, const char* str2);
+
+int main()
+{
+    const char* provinces[] = {
+        "Hubei",
+        "Hunan",
+        "Henan",
+        "Anhui",
+        "Xinjiang",
+        "Taiwan",
+        "Xizang",
+        "Neimenggu"
+    };
+
+    std::sort(std::begin(provinces), std::end(provinces), compare);
+
+    for (auto province : provinces)
+    {
+        std::cout << province << std::endl;
+    }
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+bool compare(const char* str1, const char* str2)
+{
+    return strcmp(str1, str2) < 0;
+}
